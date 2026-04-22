7386799a6c4ee0bcdcb22ac8d40e24b13282d81c
SOAL Abdeldjallil <abdeldjallil93@hotmail.fr>
2026-04-22 18:18:55 +0100

    Rename migration file to anpr_universel

---
+++ migration_parking_anpr_universel.sql
@@ -0,0 +1,22 @@
+CREATE TABLE currentpictures (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    plate VARCHAR(20),                -- Ajustez la taille selon vos besoins
+    Picture LONGBLOB,                 -- Stockage de l'image principale
+    PlatePicture LONGBLOB,            -- Stockage de l'image de la plaque
+    pic_size INT,                     -- Taille en octets ou dimensions
+    plate_pic_size INT,               -- Taille en octets ou dimensions
+    time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE `database_manager`.`parkingaccesslog` 
+ADD COLUMN `QuitPlatePicture` LONGBLOB NULL AFTER `lpnOUT`;
+
+ALTER TABLE `database_manager`.`parkingaccesslog` 
+ADD COLUMN `PlatePicture` LONGBLOB NULL AFTER `lpnOUT`;
+
+
+ALTER TABLE `database_manager`.`currentpictures` 
+ADD COLUMN `plate_pic_size` INT NOT NULL DEFAULT 0 AFTER `plate`;
+
+ALTER TABLE `database_manager`.`currentpictures` 
+ADD COLUMN `PlatePicture` LONGBLOB NULL AFTER `plate`;
