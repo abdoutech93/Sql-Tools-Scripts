b9e5b046d859b434536046df0a88f6a5f60d1547
SOAL Abdeldjallil <abdeldjallil93@hotmail.fr>
2026-04-26 12:36:39 +0100

    Change observation column to TEXT with default value

--- migration_parking_anpr_universel.sql
+++ migration_parking_anpr_universel.sql
@@ -18,6 +18,9 @@ ADD COLUMN `PlatePicture` LONGBLOB NULL AFTER `lpnOUT`;
 ALTER TABLE `database_manager`.`parkingaccesslog` 
 ADD COLUMN `plate_pic_size` INT NOT NULL DEFAULT 0 ;
 
+ALTER TABLE `database_manager`.`parkingaccesslog` 
+CHANGE COLUMN `observation` `observation` TEXT NOT NULL DEFAULT '' ;
+
 ALTER TABLE `database_manager`.`currentpictures` 
 ADD COLUMN `plate_pic_size` INT NOT NULL DEFAULT 0 AFTER `plate`;
 
